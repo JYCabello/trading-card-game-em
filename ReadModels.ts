b37664b0ad9de3a1c90038fc1761acedda0751b3
Gildas Quéméner <gildas.quemener@codurance.com>
2023-01-09 15:17:34 +0100

    Inform typescript that object is readonly
    Typescript does not seem to infer type, that's why it's not informing us
    that readonly object is being mutated.

--- ReadModels.ts
+++ ReadModels.ts
@@ -1,27 +1,27 @@
 import * as Events from "./Events"
 
-export type GameState = {
-  readonly myself: {
-    readonly hp: number;
-    readonly deck: ReadonlyArray<number>;
-    readonly hand: ReadonlyArray<number>;
-    readonly manaSlots: number;
-    readonly manaSpent: number;
-  },
-  readonly opponent: {
-    readonly hp: number;
-    readonly deckSize: number;
-    readonly handSize: number;
-    readonly manaSlots: number;
-    readonly manaSpent: number;
-  }
-}
+export type GameState = Readonly<{
+  myself: Readonly<{
+    hp: number;
+    deck: ReadonlyArray<number>;
+    hand: ReadonlyArray<number>;
+    manaSlots: number;
+    manaSpent: number;
+  }>,
+  opponent: Readonly<{
+    hp: number;
+    deckSize: number;
+    handSize: number;
+    manaSlots: number;
+    manaSpent: number;
+  }>
+}>
 
 export function buildStateFor(
   playerID: number,
   events: ReadonlyArray<Events.GameEvent>
   ): GameState {
-    const x = {
+    const x: GameState = {
       myself: {
         hp: 30,
         deck: [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8],
