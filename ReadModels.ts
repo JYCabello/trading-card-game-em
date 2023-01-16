d2105128c521fede991bd808ed4b5417d8644e14
yeray <yeray.cabello@codurance.com>
2023-01-16 14:30:17 +0100

    Using the discriminated union to fold.

--- ReadModels.ts
+++ ReadModels.ts
@@ -22,7 +22,8 @@ export function buildStateFor(
   playerID: number,
   events: ReadonlyArray<Events.GameEvent>
   ): GameState {
-    return {
+
+    const initialState : GameState = {
       myself: {
         hp: 30,
         deck: [0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8],
@@ -39,4 +40,26 @@ export function buildStateFor(
       },
       isMyTurn: false
     }
+
+    const x : GameState = events.reduce((acc, evt) => {
+      switch (evt.type) {
+        case "GameStarted":
+          throw "TODO";
+        case "FirstPlayerPicked":
+          throw "TODO";
+        case "TurnStarted":
+          throw "TODO";
+        case "ManaSlotGranted":
+          throw "TODO";
+        case "ManaRefilled":
+          throw "TODO";
+        case "CardDrawn":
+          throw "TODO";
+      
+        default:
+          const _: never = evt
+      }
+    }, initialState);
+
+    return initialState;
 }
